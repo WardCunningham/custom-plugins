d92478eeed40ad8cc1cf59a38020cee028d40784
Ward Cunningham <ward@c2.com>
2016-02-22 06:01:14 -0800

    Update install-plugins.sh

--- install-plugins.sh
+++ install-plugins.sh
@@ -1,3 +1,6 @@
 # install custom plugins
 # usage: curl https://raw.githubusercontent.com/WardCunningham/custom-plugins/master/install-plugins.sh | sh
+npm i wiki-plugin-cypher --save
+npm i wiki-plugin-datscript --save
 npm i wiki-plugin-morseteacher --save
+npm i wiki-plugin-plugins --save
